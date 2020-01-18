75bc94174292039ffc1c3a307f70037a445d0076
unknown <cdwhelan@gmail.com>
2020-01-18 15:05:03 -0500

    Start

---
+++ src/main/java/whela/ama/amazonsearch/AmazonSearchApplication.java
@@ -0,0 +1,13 @@
+package whela.ama.amazonsearch;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmazonSearchApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmazonSearchApplication.class, args);
+	}
+
+}
